ff3521e226deb4bb9717030ae726f541fd7f3877
leon_huang <leon_huang@gemteks.com>
2014-08-03 14:42:47 +0800

    [14:44] p.240-248

--- src/com/zlargon/dragdrog/MainActivity.java
+++ src/com/zlargon/dragdrog/MainActivity.java
@@ -1,7 +1,10 @@
 package com.zlargon.dragdrog;
 
 import android.app.Activity;
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ImageView1 : long click
+        ImageView imageView1 = (ImageView) findViewById(R.id.imageView1);
+        imageView1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                ClipData dragData = null;
+                MyDragShadowBuilder myDragShadowBuilder = new MyDragShadowBuilder(v);
+                v.startDrag(dragData, myDragShadowBuilder, null, 0);
+                return true;
+            }
+        });
     }
 }
---
+++ src/com/zlargon/dragdrog/MyDragShadowBuilder.java
@@ -0,0 +1,29 @@
+package com.zlargon.dragdrog;
+
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.view.View;
+import android.view.View.DragShadowBuilder;
+
+public class MyDragShadowBuilder extends DragShadowBuilder {
+
+    public MyDragShadowBuilder(View view) {
+        super(view);
+    }
+
+    @Override
+    public void onProvideShadowMetrics(Point shadowSize, Point shadowTouchPoint) {
+        // get width, height
+        int width = getView().getWidth();
+        int height = getView().getHeight();
+
+        // set shadow
+        shadowSize.set(width, height);
+        shadowTouchPoint.set(width, height);
+    }
+
+    @Override
+    public void onDrawShadow(Canvas canvas) {
+        super.onDrawShadow(canvas);
+    }
+}
